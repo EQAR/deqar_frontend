1796b8de0a8997149c097590b179292314fb24bb
Gábor <gabor.gyorgy@penceo.com>
2019-01-29 21:24:08 +0100

    feat(institutions): create institutions component

---
+++ src/views/Institutions/Institutions.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Card, CardBody} from "reactstrap";
+
+class Institutions extends Component {
+  render() {
+    return(
+      <div className="animated fadeIn">
+        <Card>
+          <CardBody>
+              hey
+          </CardBody>
+        </Card>
+      </div>
+    )
+  }
+}
+
+export default Institutions;
