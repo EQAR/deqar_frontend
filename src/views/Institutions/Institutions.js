c6f2264820e915189f90362956cfbc72691305bd
Gábor <gabor.gyorgy@penceo.com>
2019-01-29 21:32:34 +0100

    feat(institutions): add cardheader and editorconfig

--- src/views/Institutions/Institutions.js
+++ src/views/Institutions/Institutions.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { Card, CardBody} from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Row
+} from "reactstrap";
 
 class Institutions extends Component {
   render() {
     return(
       <div className="animated fadeIn">
         <Card>
+          <CardHeader>
+            <Row>
+              <Col>Institutions</Col>
+            </Row>
+          </CardHeader>
           <CardBody>
               hey
           </CardBody>
