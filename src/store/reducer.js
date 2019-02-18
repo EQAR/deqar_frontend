964ff9bdb4332e4d1cb54bb8380906b8e47b51f9
József Gábor Bóné <jozsef.bone@gmail.com>
2019-02-18 22:48:06 +0100

    New actionTypes + reducers.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,18 @@
 import { combineReducers } from "redux";
 import userReducer from "../components/DefaultLayout/reducers/userReducer";
-import reportTableReducer from "../views/Dashboard/reducers/reportTableReducer";
+import dashboardReportTableReducer from "../views/Dashboard/reducers/dashboardReportTableReducer";
 import institutionsTableReducer from "../views/Institutions/reducers/institutionsTableReducer";
+import myReportsTableReducer from "../views/MyReports/reducers/myReportsTableReducer";
+import reportsTableReducer from "../views/Reports/reducers/reportsTableReducer";
 
 // Create the combined reducer
 const reducer = combineReducers(
   {
     user: userReducer,
     institutionsTable: institutionsTableReducer,
-    reportTable: reportTableReducer
+    dashboardReportTable: dashboardReportTableReducer,
+    myReportsTable: myReportsTableReducer,
+    reportsTable: reportsTableReducer
   }
 );
 
