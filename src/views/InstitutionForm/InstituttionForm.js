39f1b7950e8c85589879ad2286c7724c79124635
Gábor <gabor.gyorgy@penceo.com>
2019-02-24 15:09:27 +0100

    feat(InstitutionForm): create form, display official name

--- src/views/InstitutionForm/InstituttionForm.js
+++ src/views/InstitutionForm/InstituttionForm.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import { Form } from 'informed';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  FormGroup, FormText,
+  Label,
+  Row
+} from "reactstrap";
 
+import FormTextField from "../../components/FormFields/FormTextField";
 import institution from "../../services/Institution";
+import style from "./InstitutionForm.module.css";
 
 
 class InstitutionForm extends Component {
@@ -19,25 +32,94 @@ class InstitutionForm extends Component {
     this.setState({
       readOnly: this.isReadOnly(formType)
     });
-    formType !== 'create' ? this.populate() : this.null();
+    this.populate();
   }
 
   isReadOnly = (formType) => formType === 'view';
 
-  null() {
-    console.log('need fix');
+  populate = () => {
+    const { formID, formType } = this.props;
 
+    if (formType !== 'create') {
+      institution.getInstitution(formID).then(response => this.formApi.setValues(response.data))
+    }
   }
 
-  populate = () => {
-    const { formID } = this.props;
-    institution.getInstitution(formID).then(response => console.log(response))
+  setFormApi = (formApi) => {
+    this.formApi = formApi;
+  };
+
+  formTitle(formType) {
+    return {
+      view: 'View Institution',
+      edit: 'Edit Institution',
+      create: 'Add Institution'
+    }[formType];
   }
 
   render() {
+    const { readOnly } = this.state;
+    const { formType } = this.props;
+
     return (
-      <div>
-        haha
+      <div className="animated fadeIn">
+        <Card>
+        <CardHeader>
+            <Row>
+              <Col>{this.formTitle(formType)}</Col>
+            </Row>
+          </CardHeader>
+          <Form
+            getApi={this.setFormApi}
+          >
+            {({ formState }) => (
+              <React.Fragment>
+                <CardBody>
+                  <Row>
+                    <Col md={6} className={style.borderLeft}>
+                      <Row>
+                        <Col>
+                          <Label for="name_official" className={'required'}>Institution Name, Official</Label>
+                            <FormTextField
+                              field={'names[0].name_official'}
+                              disabled={readOnly}
+                            />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Label for="name_primary" className={'required'}>Institution Name, Official</Label>
+                            <FormTextField
+                              field={'name_primary'}
+                              disabled={readOnly}
+                            />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Label for="name_primary" className={'required'}>Institution Name, Official</Label>
+                            <FormTextField
+                              field={'name_primary'}
+                              disabled={readOnly}
+                            />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Label for="name_primary" className={'required'}>Institution Name, Official</Label>
+                            <FormTextField
+                              field={'name_primary'}
+                              disabled={readOnly}
+                            />
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </React.Fragment>
+            )}
+          </Form>
+        </Card>
       </div>
     )
   }
