ed100da22cf3cc1f7108cfd1b469690e9a04a4d4
Gábor <gabor.gyorgy@penceo.com>
2019-02-25 22:19:06 +0100

    feat(institutionForm): add alternative names list

--- src/views/InstitutionForm/InstituttionForm.js
+++ src/views/InstitutionForm/InstituttionForm.js
@@ -17,6 +17,8 @@ import PropTypes from 'prop-types';
 import FormTextField from "../../components/FormFields/FormTextField";
 import institution from "../../services/Institution";
 import style from "./InstitutionForm.module.css";
+import AssignedList from "../../components/FormFieldsUncontrolled/AssignedList";
+
 
 
 class InstitutionForm extends Component {
@@ -57,6 +59,38 @@ class InstitutionForm extends Component {
       edit: 'Edit Institution',
       create: 'Add Institution'
     }[formType];
+  }
+
+  toggleNameModal = () => {
+    console.log('toggle');
+
+    // this.setState({
+    //   programmeModalOpen: !this.state.programmeModalOpen,
+    //   programmeModalIndex: undefined,
+    //   programmeModalValue: undefined
+    // })
+  };
+
+  onNameRemove = (index) => {
+    console.log(index);
+
+    // let programmes = this.formApi.getValue('programmes');
+    // programmes.splice(idx, 1);
+    // this.formApi.setValue('programmes', programmes);
+  }
+
+  onNameClick = (index) => {
+    console.log(index);
+
+    // this.setState({
+    //   programmeModalOpen: true,
+    //   programmeModalValue: this.formApi.getValue('programmes')[idx],
+    //   programmeModalIndex: idx
+    // });
+  };
+
+  getAlternativeValues = (formState) => {
+    return formState.values.names ? formState.values.names[0].alternative_names : null;
   }
 
   render() {
@@ -115,6 +149,22 @@ class InstitutionForm extends Component {
                             />
                         </Col>
                       </Row>
+                      <Row>
+                        <Col>
+                          <AssignedList
+                            errors={formState.errors}
+                            valueFields={['name']}
+                            values={this.getAlternativeValues(formState)}
+                            label={'Alternative Names'}
+                            btnLabel={'Add Alternative Name'}
+                            onRemove={this.onNameRemove}
+                            onAddButtonClick={this.toggleNameModal}
+                            onClick={this.onNameClick}
+                            field={'alternative_names'}
+                            disabled={readOnly}
+                          />
+                        </Col>
+                      </Row>
                     </Col>
                     <Col md={6}>
                       <Row>
@@ -128,10 +178,46 @@ class InstitutionForm extends Component {
                       </Row>
                       <Row>
                         <Col md={6}>
-                          <Label for="deqar_id">ETER ID</Label>
+                          <Label for="eter_id">ETER ID</Label>
+                            <FormTextField
+                              field={'eter_id'}
+                              disabled
+                            />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Label for="deqar_id">National Identifier</Label>
+                            <FormTextField
+                              field={'identifiers[0].identifier'}
+                              disabled={readOnly}
+                            />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Label for="deqar_id">Local Identifier</Label>
+                            <FormTextField
+                              field={'identifiers[1].identifier'}
+                              disabled={readOnly}
+                            />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Label for="deqar_id">QF-EHEA Levels</Label>
                             <FormTextField
                               field={'deqar_id'}
-                              disabled
+                              disabled={readOnly}
+                            />
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col>
+                          <Label for="website_link" className={'required'}>Institution Website</Label>
+                            <FormTextField
+                              field={'website_link'}
+                              disabled={readOnly}
                             />
                         </Col>
                       </Row>
