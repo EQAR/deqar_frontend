254ca88ae5f8728d3a43ff431d1ea7d51867267c
Gábor <gabor.gyorgy@penceo.com>
2019-02-26 22:47:13 +0100

    feat(LocationForm): create component

---
+++ src/views/InstitutionForm/components/LocationForm.js
@@ -0,0 +1,126 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Col,
+  Collapse,
+  FormGroup,
+  Label,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Row} from "reactstrap";
+import PropTypes from 'prop-types';
+import {
+  Form,
+  Scope} from 'informed';
+
+import FormTextField from "../../../components/FormFields/FormTextField";
+import { validateRequired } from "../../../utils/validators";
+
+class LocationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  setFormApi = (formApi) => {
+    const { formValue } = this.props;
+    this.formApi = formApi;
+    if (formValue) {
+      this.formApi.setValues(formValue);
+    }
+  }
+
+  submitForm = () => {
+    this.formApi.submitForm();
+  }
+
+  onAddButtonClick = () => {
+    console.log('add');
+
+  }
+
+  onToggle = () => {
+    this.props.onToggle();
+  }
+
+  renderActionName = () => {
+    const {formIndex, disabled} = this.props;
+    let action = '';
+
+    if(formIndex >= 0) {
+      action = disabled ? 'View' : 'Edit'
+    } else {
+      action = 'Add'
+    }
+
+    return action;
+  }
+
+  render() {
+    const { modalOpen, disabled, formIndex } = this.props;
+
+    return(
+      <Modal isOpen={modalOpen} toggle={this.onToggle}>
+        <Form
+          getApi={this.setFormApi}
+          onSubmit={(value) => this.props.onFormSubmit(value, formIndex)}
+          id="alternative-name-form"
+        >
+          {({ formState }) => (
+            <React.Fragment>
+              <ModalHeader toggle={this.onToggle}>Alternative Name</ModalHeader>
+              <ModalBody>
+                <Row>
+                  <Col md={12}>
+                    <FormGroup>
+                      <Label for="alternative_name">Alternative Institution Name</Label>
+                      <FormTextField
+                        field={'name'}
+                        placeholder={'Enter alternative institution name'}
+                        validate={validateRequired}
+                        disabled={disabled}
+                      />
+                    </FormGroup>
+                  </Col>
+                  <Col md={12}>
+                    <FormGroup>
+                      <Label for="name_transliterated">Alternative Institution Name, Transliterated</Label>
+                      <FormTextField
+                        field={'transliteration'}
+                        placeholder={'Enter alternative institution name, transliterated'}
+                        disabled={disabled}
+                      />
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  color="primary"
+                  type={'button'}
+                  onClick={this.props.onToggle}
+                >
+                  Close
+                </Button>
+              </ModalFooter>
+            </React.Fragment>
+          )}
+          </Form>
+      </Modal>
+    )
+  }
+}
+
+LocationForm.propTypes = {
+  modalOpen: PropTypes.bool.isRequired,
+  formValue: PropTypes.object,
+  onToggle: PropTypes.func.isRequired,
+  onFormSubmit: PropTypes.func.isRequired,
+  disabled: PropTypes.bool
+}
+
+export default LocationForm;
