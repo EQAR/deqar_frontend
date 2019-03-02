c53caba425f2761d49c026877caa6ca5eed85946
József Gábor Bóné <jozsef.bone@gmail.com>
2019-03-02 01:13:30 +0100

    fix(FormDatePickerField): Remove placeholderText when the field is disabled.

--- src/components/FormFields/FormDatePickerField.js
+++ src/components/FormFields/FormDatePickerField.js
@@ -8,7 +8,7 @@ import "react-datepicker/dist/react-datepicker.css";
 const FormDatePickerField = asField(({ fieldState, fieldApi, ...props }) => {
   const { value } = fieldState;
   const { setValue, setTouched } = fieldApi;
-  const { onChange, onBlur, initialValue,...rest } = props;
+  const { onChange, onBlur, initialValue, placeholderText, disabled,...rest } = props;
 
   const setDateValue = (value) => {
     return value ? moment(value).format('YYYY-MM-DD') : null;
@@ -26,6 +26,8 @@ const FormDatePickerField = asField(({ fieldState, fieldApi, ...props }) => {
         {...rest}
         dateFormat="yyyy-MM-dd"
         className={fieldState.error ? 'form-control is-invalid' : 'form-control'}
+        disabled={disabled}
+        placeholderText={disabled ? '' : placeholderText}
         onSelect={(value) => {
           setValue(setDateValue(value));
           if (onChange) {
