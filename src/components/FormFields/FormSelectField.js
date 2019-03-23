b4889a66cc6292ec2d3bf1bdad704dcb3cf9329b
József Gábor Bóné <jozsef.bone@gmail.com>
2019-03-23 22:50:31 +0100

    feat(FormSelectField): Fix#17 ID numbers are displayed as badge.

--- src/components/FormFields/FormSelectField.js
+++ src/components/FormFields/FormSelectField.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { asField } from 'informed';
 import Select from 'react-select';
+import {Badge} from "reactstrap";
+import style from './FormSelectField.module.scss';
 
 const FormSelectField = asField(({ fieldState, fieldApi, ...props }) => {
   const { value } = fieldState;
   const { setValue, setTouched, setError } = fieldApi;
   const { onChange, onBlur, initialValue, forwardedRef, labelField, valueField, disabled, placeholder, ...rest } = props;
+
+  const includeID = props.includeID ? props.includeID : false;
 
   const borderColor = fieldApi.getError() ? '#f86c6b' : '#e4e7ea';
 
@@ -41,6 +45,14 @@ const FormSelectField = asField(({ fieldState, fieldApi, ...props }) => {
     })
   };
 
+  const getLabel = (option) => {
+    if(includeID) {
+      return (<React.Fragment>{option[labelField]} <Badge className={style.Badge}>{option['id']}</Badge></React.Fragment>)
+    } else {
+      return option[labelField]
+    }
+  };
+
   return (
     <React.Fragment>
       <Select
@@ -65,7 +77,7 @@ const FormSelectField = asField(({ fieldState, fieldApi, ...props }) => {
         isDisabled={disabled}
         placeholder={disabled ? "" : placeholder}
         isClearable={true}
-        getOptionLabel={(option) => {return option[labelField]}}
+        getOptionLabel={getLabel}
         getOptionValue={(option) => {return option[valueField]}}
       />
       {fieldState.error ? (
