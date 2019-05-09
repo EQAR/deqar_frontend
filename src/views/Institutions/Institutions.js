bf08f93fe53f66b33f52418f7e422ac4e78b900c
József Gábor Bóné <jozsef.bone@gmail.com>
2019-05-09 15:41:22 +0200

    feat(InstitutionsTable): Fix#10 Add new filter headers.

--- src/views/Institutions/Institutions.js
+++ src/views/Institutions/Institutions.js
@@ -2,33 +2,50 @@ import React, { Component } from 'react';
 import {
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
-  Col,
-  Row
 } from "reactstrap";
 import InstitutionsTable from './InstitutionsTable';
 import style from "./Institutions.module.css"
+import toggleInstitutionsTableFilter from "./actions/toggleInstitutionsTableFilter";
+import DataTableHeader from "../../components/DataTable/DataTableHeader";
+import {connect} from "react-redux";
 
 class Institutions extends Component {
+  onFilterClick = () => {
+    this.props.toggleInstitutionsTableFilter()
+  };
+
   render() {
     return(
       <div className="animated fadeIn">
         <Card>
           <CardHeader>
-            <Row>
-              <Col>Institutions</Col>
-            </Row>
+            Institutions
+            <DataTableHeader
+              storeName={'institutionsTable'}
+              filterText={'Number of institutions'}
+              onFilterClick={this.onFilterClick}
+            />
           </CardHeader>
           <CardBody className={style.InstitutionsCardBody}>
-            <InstitutionsTable />
+            <InstitutionsTable/>
           </CardBody>
-          <CardFooter>
-          </CardFooter>
         </Card>
       </div>
     )
   }
 }
 
-export default Institutions;
+const mapStateToProps = (store) => {
+  return {}
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleInstitutionsTableFilter: state => {
+      dispatch(toggleInstitutionsTableFilter())
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Institutions);
--- src/views/Institutions/actions/setInstitutionsTable.js
+++ src/views/Institutions/actions/setInstitutionsTable.js
@@ -1,6 +1,6 @@
 import { SET_INSTITUTIONS_TABLE } from "../../../store/actionTypes";
 
-export default function setInstitutionTable(state) {
+export default function setInstitutionsTable(state) {
   return {
     type: SET_INSTITUTIONS_TABLE,
     payload: { state: state }
