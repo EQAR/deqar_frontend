63da7bafad0d0fff04e3d5bf747f3650c7b03a22
József Gábor Bóné <jozsef.bone@gmail.com>
2019-06-01 09:53:34 +0200

    feat(InfoBox): Proper display of update logs

--- src/views/ReportForm/components/InfoBox.js
+++ src/views/ReportForm/components/InfoBox.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Col from "reactstrap/es/Col";
 import Row from "reactstrap/es/Row";
-
+import { Text } from "informed";
 import style from "./InfoBox.module.css"
 import {FormGroup, ListGroup} from "reactstrap";
 import Label from "reactstrap/es/Label";
@@ -10,6 +9,7 @@ import ListGroupItem from "reactstrap/es/ListGroupItem";
 import moment from "moment";
 import FormTextField from "../../../components/FormFields/FormTextField";
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 class InfoBox extends Component {
   flagRender = (value) => {
@@ -34,7 +34,7 @@ class InfoBox extends Component {
     const { formState } = this.props;
     if(Object.entries(formState).length !== 0) {
       const {flags} = formState;
-      if(flags.length > 0) {
+      if(flags && flags.length > 0) {
         return flags.map((flag, idx) => {
           if(flag.active) {
             return(
@@ -89,6 +89,33 @@ class InfoBox extends Component {
     }
   };
 
+  renderUpdateLog = () => {
+    const {formState} = this.props;
+
+    if (Object.entries(formState).length !== 0) {
+      const updateLogs = formState['update_log'];
+      if(updateLogs) {
+        return(
+          <React.Fragment>
+            {updateLogs.map((updateLog, idx) => {
+              const updatedBy = updateLog.updated_by ? `by '${updateLog.updated_by}'` : '';
+              const note = updateLog.note ? `(${updateLog.note})` : '';
+
+              return(
+                <input
+                  key={idx}
+                  className={cx(style.infoInput, 'form-control')}
+                  disabled={true}
+                  value={`Updated: ${this.renderDate(updateLog.updated_at)} ${updatedBy} ${note}`}
+                />
+              )
+            })}
+          </React.Fragment>
+        )
+      }
+    }
+  };
+
   renderDate = (date) => {
     if(date) {
       return moment(date, moment.ISO_8601).format("YYYY-MM-DD HH:mm:ss")
@@ -110,6 +137,7 @@ class InfoBox extends Component {
             </FormGroup>
             <FormGroup>
               <Label>Current flags</Label>
+              <Text field={'flags'} hidden={true}/>
               <ListGroup>
                 {this.renderFlags()}
               </ListGroup>
@@ -127,16 +155,15 @@ class InfoBox extends Component {
             </FormGroup>
             <FormGroup>
               <Label>Record History</Label>
+              <Text field={'update_log'} hidden={true}/>
+              {this.renderUpdateLog()}
+              <Text field={'created_at'} hidden={true}/>
+              <Text field={'created_by'} hidden={true}/>
               <input
                 className={cx(style.infoInput, 'form-control')}
                 disabled={true}
-                value={`Created at ${this.renderDate(formState.created_at)} by '${formState.created_by}'`}
+                value={`Record created: ${this.renderDate(formState.created_at)} by '${formState.created_by}'`}
 
-              />
-              <input
-                className={cx(style.infoInput, 'form-control')}
-                disabled={true}
-                value={`Updated at ${this.renderDate(formState.updated_at)} by '${formState.updated_by}'`}
               />
             </FormGroup>
           </Col>
