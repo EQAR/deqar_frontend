c94621efa8a7fbac35f3a40e13456cac6fca4fae
József Gábor Bóné <jozsef.bone@gmail.com>
2019-06-01 09:52:32 +0200

    feat(ReportForm): Update report submission button.

--- src/views/ReportForm/ReportForm.js
+++ src/views/ReportForm/ReportForm.js
@@ -7,8 +7,7 @@ import {
   CardFooter,
   CardHeader,
   Col, Collapse,
-  FormGroup, FormText,
-  Label,
+  FormGroup, FormText, Label,
   Row
 } from "reactstrap";
 import style from './ReportForm.module.css'
@@ -54,7 +53,8 @@ class ReportForm extends Component {
       nonFieldErrors: [],
       loading: false,
       readOnly: false,
-      infoBoxOpen: true
+      infoBoxOpen: true,
+      submitMessageOpen: false
     }
   }
 
@@ -254,6 +254,7 @@ class ReportForm extends Component {
     })
   };
 
+
   // Toggle loading
   loadingToggle = () => {
     this.setState({
@@ -268,6 +269,12 @@ class ReportForm extends Component {
   };
 
   // Events
+  onSubmitClick = () => {
+    this.setState({
+      submitMessageOpen: true
+    })
+  };
+
   onAlertClose = () => {
     this.setState({
       alertVisible: false
@@ -361,10 +368,8 @@ class ReportForm extends Component {
   };
 
   onFileAdded = (file) => {
-    console.log(file);
     if(file.length > 0) {
       if (file[0] instanceof File) {
-        console.log('add');
         this.setState({
           uploadedFile: file[0]
         });
@@ -521,6 +526,9 @@ class ReportForm extends Component {
   updateReport = (values) => {
     const { reportID } = this.props;
     let normalizedForm = updateFormNormalizer(values);
+
+    this.loadingToggle();
+
     normalizedForm = encodeProgrammeNameData(normalizedForm);
     report.updateReport(normalizedForm, reportID).then((response) => {
       toast.success("Report record was updated.");
@@ -532,12 +540,14 @@ class ReportForm extends Component {
     }).then(() => {
       this.loadingToggle();
       this.populateForm()
-    })
+    }).catch(error => {
+      this.loadingToggle();
+      console.log(error.response)
+    });
   };
 
   onSubmit = (values) => {
     const {formType} = this.props;
-    this.loadingToggle();
     switch(formType) {
       case 'create':
         this.createReport(values);
@@ -550,7 +560,36 @@ class ReportForm extends Component {
     }
   };
 
-  renderSubmitButton = () => {
+  renderEditSubmitButton = () => {
+    const {submitMessageOpen} = this.state;
+
+    if (submitMessageOpen) {
+      return (
+        <div className={'pull-right'}>
+          <LaddaButton
+            className={style.reportSubmitButton + " btn btn-primary btn-ladda btn-sm"}
+            loading={this.state.loading}
+            data-color="blue"
+            data-style={EXPAND_RIGHT}
+            onClick={() => this.formApi.submitForm()}
+          >Submit</LaddaButton>
+        </div>
+      )
+    } else {
+      return(
+        <div className={'pull-right'}>
+          <Button
+            type={'button'}
+            size="sm"
+            color="primary"
+            onClick={this.onSubmitClick}
+          >Submit</Button>
+        </div>
+      )
+    }
+  };
+
+  renderCreateSubmitButton = () => {
     return(
       <div className={'pull-right'}>
         <LaddaButton
@@ -611,6 +650,21 @@ class ReportForm extends Component {
     }
   };
 
+  renderSubmitMessage = () => {
+    const {submitMessageOpen} = this.state;
+
+    return (
+      <Collapse isOpen={submitMessageOpen}>
+        <FormGroup>
+          <FormTextField
+            field={'submit_message'}
+            placeholder={'Please enter a short description of your edit... (Optional)'}
+          />
+        </FormGroup>
+      </Collapse>
+    )
+  };
+
   renderButtons = () => {
     const {formType} = this.props;
 
@@ -627,14 +681,15 @@ class ReportForm extends Component {
         return(
             <Row>
               <Col xs={12}>
-                {this.renderSubmitButton()}
+                {this.renderCreateSubmitButton()}
               </Col>
             </Row>
         );
       case 'edit':
         return(
           <div>
-            {this.renderSubmitButton()}
+            {this.renderSubmitMessage()}
+            {this.renderEditSubmitButton()}
             {this.renderCloseButton()}
             {this.renderHideInfoButton()}
           </div>
