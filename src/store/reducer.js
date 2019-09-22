d9b289432d98e85974d22070e007bfeb641cc916
József Gábor Bóné <jozsef.bone@gmail.com>
2019-09-22 18:22:30 +0200

    fix(Refactor): Remove unnecessary files.

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import userReducer from "../components/DefaultLayout/reducers/userReducer";
-import dashboardReportTableReducer from "../views/Dashboard/reducers/dashboardReportTableReducer";
 import institutionsTableReducer from "../views/Institutions/reducers/institutionsTableReducer";
 import myReportsTableReducer from "../views/MyReports/reducers/myReportsTableReducer";
 import reportsTableReducer from "../views/Reports/reducers/reportsTableReducer";
@@ -13,7 +12,6 @@ const reducer = combineReducers(
   {
     user: userReducer,
     institutionsTable: institutionsTableReducer,
-    dashboardReportTable: dashboardReportTableReducer,
     myReportsTable: myReportsTableReducer,
     reportsTable: reportsTableReducer,
     agencyActivitiesTable: agencyActivitiesTableReducer,
