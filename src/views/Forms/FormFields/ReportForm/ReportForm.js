436df0c34b1bbd92b98f6ecfc7698f5cfa703d13
Josh <jozsef.bone@gmail.com>
2025-03-12 20:52:49 +0100

    Update ReportForm.js

--- src/views/Forms/FormFields/ReportForm/ReportForm.js
+++ src/views/Forms/FormFields/ReportForm/ReportForm.js
@@ -27,43 +27,24 @@ import FormTextAreaFormatted from "../../../../components/FormFields/FormTextAre
 import FormCheckbox from "../../../../components/FormFields/FormCheckbox/FormCheckbox";
 import validateStatus from "./validators/validateStatus";
 import validateMicroCredentials from "./validators/validateMicroCredentials";
-import {detectActivityType} from "../../../../utils/detectActivityType";
-import validateActivities from "./validators/validateActivities";
 
 const ReportForm = ({formType, formApi, formState, readOnly}) => {
   const isAgencyDisabled = () => {
     return formType !== 'create';
   };
 
-  const onInstitutionSelected = (value) => {
-    let institutions = formApi.getValue('institutions');
-    if (institutions) {
+  const onInstitutionSelected = (value, field='institutions') => {
+    let institutions = formApi.getValue(field);
+    if(institutions) {
       const institution_ids = institutions.map(i => i.id.toString());
-      if (!institution_ids.includes(value.id.toString())) {
+      if(!(institution_ids.includes(value.id))) {
         institutions.push(value)
       }
     } else {
       institutions = [value]
     }
-    formApi.setValue('institutions', institutions);
+    formApi.setValue(field, institutions);
   };
-
-  const onActivitySelected = (value) => {
-    let activities = formApi.getValue('activities');
-
-    if (activities) {
-      const activity_ids = activities.map(i => i.id.toString());
-
-      if (!(activity_ids.includes(value.id.toString()))) {
-        activities.push(value)
-      }
-    } else {
-      activities = [value]
-    }
-    formApi.setValue('activities', activities);
-  };
-
-
 
   // Report Links
   const renderLinksDisplayValue = (value) => {
@@ -106,252 +87,249 @@ const ReportForm = ({formType, formApi, formState, readOnly}) => {
   };
 
   const renderProgrammeField = () => {
-    const activities = formState.values['activities'];
-    const activityType = detectActivityType(activities);
-    if (activityType !== 'institutional') {
-      return(
-        <PopupFormListManager
-          field={'programmes'}
-          formApi={formApi}
-          renderDisplayValue={renderProgrammeDisplayValue}
-          labelShowRequired={true}
-          disabled={readOnly}
-          validate={validateProgrammes}
-        >
-          <ProgrammeSubform size={'lg'} institutions={formState.values['institutions']} />
-        </PopupFormListManager>
-      )
+    const activity = formState.values['activity'];
+    if (activity) {
+      const activityType = activity.activity_type;
+      if(activityType !== 'institutional') {
+        return(
+            <PopupFormListManager
+                field={'programmes'}
+                formApi={formApi}
+                renderDisplayValue={renderProgrammeDisplayValue}
+                labelShowRequired={true}
+                disabled={readOnly}
+                validate={validateProgrammes}
+            >
+              <ProgrammeSubform size={'lg'} institutions={formState.values['institutions']} />
+            </PopupFormListManager>
+        )
+      }
     }
   };
 
-  const getAgencyValues = () => {
-    const agencies = [];
-    const agency = formState.values['agency'];
-    const contributing_agencies = formState.values['contributing_agencies'];
-
-    agency && agencies.push(agency.id);
-
-    if (contributing_agencies) {
-      contributing_agencies.forEach((contributing_agency) => {
-        agencies.push(contributing_agency.id);
-      });
-    }
-    return agencies;
-  }
-
   return(
-    <Row>
-      <Col md={6} className={style.reportFormLeft}>
-        <Row>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="agency" className={'required'}>Agency</Label>
-              <FormSelectField
-                field={'agency'}
-                placeholder={'Select agency...'}
-                optionsAPI={agency.selectMySubmissionAgency}
-                labelField={'acronym_primary'}
-                valueField={'id'}
-                validate={validateRequired}
-                disabled={isAgencyDisabled()}
-              />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="local_identifier">Local Report Identifier</Label>
-              <FormTextField
-                field={'local_identifier'}
-                placeholder={'Enter local report ID'}
-                disabled={readOnly}
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <FormGroup>
-              <Label for="contributing_agencies">Contributing Agencies</Label>
-              <FormSelectField
-                field={'contributing_agencies'}
-                optionsAPI={agency.selectAllAgencies}
-                placeholder={'Please select multiple, if necessary'}
-                labelField={'acronym_primary'}
-                valueField={'id'}
-                isMulti
-                disabled={readOnly}
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12}>
-            <FormGroup>
-              <Label for="summary">Summary</Label>
-              <FormTextAreaFormatted
-                field={'summary'}
-                disabled={readOnly}
-                height={'100px'}
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        { readOnly ? "" :
-            <Row>
-              <Col md={12}>
-                <FormGroup>
-                  <Label for="activities" className={'required'}>Activities</Label>
-                  <FormDependentSelectField
-                      field={''}
-                      emptyAfterChange={true}
-                      placeholder={'Select agency ESG activity...'}
-                      optionsAPI={agency.selectActivityByAgencies}
-                      optionsID={getAgencyValues()}
-                      labelFunction={(option) => `${option['activity']} (${option['agency']}) - ID ${option['id']}`}
-                      valueField={'id'}
-                      disabled={readOnly}
-                      onChange={onActivitySelected}
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
-        }
-        <Row>
-          <Col md={12}>
-            { readOnly ? <Label for="activities" className={'required'}>Activities</Label> : null}
-            <FormAssignedList
-                field={'activities'}
-                labelShowRequired={true}
-                renderDisplayValue={(value) => (`${value['activity']} (${value['agency']}) - ID ${value['id']}`)}
-                validate={validateActivities}
-                disabled={readOnly}
-            />
-          </Col>
-        </Row>
-        { readOnly ? "" :
+      <Row>
+        <Col md={6} className={style.reportFormLeft}>
           <Row>
-            <Col md={12}>
+            <Col md={6}>
               <FormGroup>
-                <Label for="institution" className={'required'}>Institutions</Label>
-                <InstitutionSelect
-                  onChange={onInstitutionSelected}
+                <Label for="agency" className={'required'}>Agency</Label>
+                <FormSelectField
+                    field={'agency'}
+                    placeholder={'Select agency...'}
+                    optionsAPI={agency.selectMySubmissionAgency}
+                    labelField={'acronym_primary'}
+                    valueField={'id'}
+                    validate={validateRequired}
+                    disabled={isAgencyDisabled()}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="local_identifier">Local Report Identifier</Label>
+                <FormTextField
+                    field={'local_identifier'}
+                    placeholder={'Enter local report ID'}
+                    disabled={readOnly}
                 />
               </FormGroup>
             </Col>
           </Row>
-        }
-        <Row>
-          <Col md={12}>
-            { readOnly ? <Label for="institution" className={'required'}>Institutions</Label> : null}
-            <FormAssignedList
-              field={'institutions'}
-              validate={validateInstitutions}
+          <Row>
+            <Col md={12}>
+              <FormGroup>
+                <Label for="contributing_agencies">Contributing Agencies</Label>
+                <FormSelectField
+                    field={'contributing_agencies'}
+                    optionsAPI={agency.selectAllAgencies}
+                    placeholder={'Please select multiple, if necessary'}
+                    labelField={'acronym_primary'}
+                    valueField={'id'}
+                    isMulti
+                    disabled={readOnly}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+              <FormGroup>
+                <Label for="summary">Summary</Label>
+                <FormTextAreaFormatted
+                    field={'summary'}
+                    disabled={readOnly}
+                    height={'100px'}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={12}>
+              <FormGroup>
+                <Label for="activity" className={'required'}>Activity</Label>
+                <FormDependentSelectField
+                    field={'activity'}
+                    placeholder={'Select agency ESG activity...'}
+                    optionsAPI={agency.selectActivity}
+                    optionsID={formState.values['agency'] ? formState.values['agency']['id'] : 0}
+                    labelField={'activity'}
+                    valueField={'id'}
+                    validate={validateRequired}
+                    disabled={readOnly}
+                    includeID={'back'}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          { readOnly ? "" :
+              <Row>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for="institution" className={'required'}>Education Provider(s)</Label>
+                    <InstitutionSelect
+                        onChange={onInstitutionSelected}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+          }
+          <Row>
+            <Col md={12}>
+              { readOnly ? <Label for="institution" className={'required'}>Education Provider(s)</Label> : null}
+              <FormAssignedList
+                  field={'institutions'}
+                  validate={validateInstitutions}
+                  labelShowRequired={true}
+                  renderDisplayValue={(value) => (value['name_primary'])}
+                  onClick={(idx) => console.log(idx)}
+                  disabled={readOnly}
+              />
+            </Col>
+          </Row>
+          { readOnly ? "" :
+              <Row>
+                <Col md={12}>
+                  <FormGroup>
+                    <Label for="platform">Platforms</Label>
+                    <InstitutionSelect
+                        onChange={(value) => onInstitutionSelected(value, 'platforms')}
+                    />
+                  </FormGroup>
+                </Col>
+              </Row>
+          }
+          <Row>
+            <Col md={12}>
+              { readOnly ? <Label for="platform">Platforms</Label> : null}
+              <FormAssignedList
+                  field={'platforms'}
+                  labelShowRequired={false}
+                  renderDisplayValue={(value) => (value['name_primary'])}
+                  onClick={(idx) => console.log(idx)}
+                  disabled={readOnly}
+              />
+            </Col>
+          </Row>
+          {renderProgrammeField()}
+        </Col>
+        <Col md={6}>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="status" className={'required'}>Status</Label>
+                <FormSelectField
+                    field={'status'}
+                    placeholder={'Select status...'}
+                    optionsAPI={report.selectStatus}
+                    labelField={'status'}
+                    valueField={'id'}
+                    includeID={'front'}
+                    validate={validateStatus}
+                    disabled={readOnly}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="decision" className={'required'}>Decision</Label>
+                <FormSelectField
+                    field={'decision'}
+                    placeholder={'Select decision...'}
+                    optionsAPI={report.selectDecisions}
+                    labelField={'decision'}
+                    valueField={'id'}
+                    includeID={'front'}
+                    validate={validateRequired}
+                    disabled={readOnly}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="valid_from" className={'required'}>Valid from</Label>
+                <FormDatePickerField
+                    field={'valid_from'}
+                    validate={(value) => validateDateFromRequired(
+                        value,
+                        formState.values.valid_to,
+                        "Report valid from date should be earlier than valid to date!"
+                    )}
+                    placeholderText={'YYYY-MM-DD'}
+                    disabled={readOnly}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="valid_to">Valid to</Label>
+                <FormDatePickerField
+                    field={'valid_to'}
+                    validate={validateDate}
+                    placeholderText={'YYYY-MM-DD'}
+                    disabled={readOnly}
+                    yearPlusValue={formState.values['valid_from']}
+                    yearPlus={true}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <PopupFormListManager
+              label={'Files'}
+              field={'report_files'}
+              formApi={formApi}
+              renderDisplayValue={renderFilesDisplayValue}
               labelShowRequired={true}
-              renderDisplayValue={(value) => (value['name_primary'])}
-              onClick={(idx) => console.log(idx)}
+              validate={validateRequired}
               disabled={readOnly}
-            />
-          </Col>
-        </Row>
-        {renderProgrammeField()}
-      </Col>
-      <Col md={6}>
-        <Row>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="status" className={'required'}>Status</Label>
-              <FormSelectField
-                field={'status'}
-                placeholder={'Select status...'}
-                optionsAPI={report.selectStatus}
-                labelField={'status'}
-                valueField={'id'}
-                includeID={'front'}
-                validate={validateStatus}
-                disabled={readOnly}
-              />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="decision" className={'required'}>Decision</Label>
-              <FormSelectField
-                field={'decision'}
-                placeholder={'Select decision...'}
-                optionsAPI={report.selectDecisions}
-                labelField={'decision'}
-                valueField={'id'}
-                includeID={'front'}
-                validate={validateRequired}
-                disabled={readOnly}
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="valid_from" className={'required'}>Valid from</Label>
-              <FormDatePickerField
-                field={'valid_from'}
-                validate={(value) => validateDateFromRequired(
-                  value,
-                  formState.values.valid_to,
-                  "Report valid from date should be earlier than valid to date!"
-                  )}
-                placeholderText={'YYYY-MM-DD'}
-                disabled={readOnly}
-              />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Label for="valid_to">Valid to</Label>
-              <FormDatePickerField
-                field={'valid_to'}
-                validate={validateDate}
-                placeholderText={'YYYY-MM-DD'}
-                disabled={readOnly}
-                yearPlusValue={formState.values['valid_from']}
-                yearPlus={true}
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-        <PopupFormListManager
-          label={'Files'}
-          field={'report_files'}
-          formApi={formApi}
-          renderDisplayValue={renderFilesDisplayValue}
-          labelShowRequired={true}
-          validate={validateRequired}
-          disabled={readOnly}
-        >
-          <FileSubform />
-        </PopupFormListManager>
-        <PopupFormListManager
-          field={'report_links'}
-          formApi={formApi}
-          renderDisplayValue={renderLinksDisplayValue}
-          labelShowRequired={false}
-          disabled={readOnly}
-        >
-          <ReportLinkSubform />
-        </PopupFormListManager>
-        <Row>
-          <Col md={12}>
-            <FormGroup>
-              <Label for="link">Other comment (optional)</Label>
-              <FormTextField
-                field={'other_comment'}
-                placeholder={'Enter comment, if necessary'}
-                disabled={readOnly}
-              />
-            </FormGroup>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
+          >
+            <FileSubform />
+          </PopupFormListManager>
+          <PopupFormListManager
+              field={'report_links'}
+              formApi={formApi}
+              renderDisplayValue={renderLinksDisplayValue}
+              labelShowRequired={false}
+              disabled={readOnly}
+          >
+            <ReportLinkSubform />
+          </PopupFormListManager>
+          <Row>
+            <Col md={12}>
+              <FormGroup>
+                <Label for="link">Other comment (optional)</Label>
+                <FormTextField
+                    field={'other_comment'}
+                    placeholder={'Enter comment, if necessary'}
+                    disabled={readOnly}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
   )
 };
 
